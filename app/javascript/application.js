3800d576585a7031ff8fc05e98a5db6ec4dbb054
Andres Muñoz <andrefe91@gmail.com>
2024-09-04 17:11:23 -0500

    Configuring React as your Rails Frontend

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,10 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
-import * as bootstrap from "bootstrap"
+import "@hotwired/turbo-rails";
+import "./controllers";
+import * as bootstrap from "bootstrap";
+import "./components";
+
+
+// import "./components/Home"
+// import "./components/App"
+// import "./components/Index"
