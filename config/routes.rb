591780b5531bb6e9f3f3c60d1b6003a1dbb3be43
Andres Muñoz <andrefe91@gmail.com>
2024-09-04 18:00:23 -0500

    Creating the Recipe Controller and Model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get "recipes/index"
+      post "recipes/create"
+      get '/show/:id', to: "recipes#show"
+      delete 'destroy/:id', to: "recipes/destroy"
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -11,4 +19,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "homepage#index"
+  get '/*path' => 'homepage#index'
 end
